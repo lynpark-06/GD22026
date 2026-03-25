a6e1fd4745a5bc8c8ffa14d3a5b2e357f21e74ac
lynpark-06 <slmagazine061016@gmail.com>
2026-03-25 15:49:34 -0400

    ss
    s

--- demos/02 - filtering/js/script.js
+++ demos/02 - filtering/js/script.js
@@ -1,4 +1,7 @@
-//leaflet
-//map libre
-//add custom marker
-//add cusgtom popup
+$(".filters button").click(function(){
+    f = $(this).data("year");
+    $(f).fadeToggle(500);
+})
+
+
+    $(".record").draggable()
--- demos/03 - leaflet/js/script.js
+++ demos/03 - leaflet/js/script.js
@@ -1,4 +1,17 @@
 //leaflet
 //map libre
 //add custom marker
-//add cusgtom popup
+//add custom popup
+
+
+var map = L.map('map').setView([43.6530098,-79.3913045], 17);
+
+L.maplibreGL({
+  style: 'https://alisq.github.io/GD22026/demos/03%20-%20leaflet/js/style.json',
+  maplibreGL: maplibregl
+}).addTo(map);
+
+  // Optional Leaflet marker on top
+    L.marker([43.6532, -79.3832])
+      .addTo(map)
+      .bindPopup('Toronto');
